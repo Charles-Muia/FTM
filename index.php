1b063ccf3afda08c605943e6b7a35a8658054b90
Charles-Muia <Charles.K.Muia@gmail.com>
2023-04-23 17:04:03 +0300

    FTM Initial Commit

---
+++ index.php
@@ -0,0 +1,131 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Fitness Touch By Mbijiwe | Home </title>
+<!----------------------------------------------------CSS--------------------------------------------------------->
+    <link rel="stylesheet" href="style.css">
+
+<!----------------------------------------------------Favicon----------------------------------------------------->
+
+    <link rel="icon" href="Gallery/Logo.jpg" id="web-icon">
+
+<!----------------------------------------------------CDN--------------------------------------------------------->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+
+</head>
+<body>
+
+<!--------------------------------------------------Navigation---------------------------------------------------->
+        <nav>
+            <div class="container nav-container" >
+<!----------------------------------------------------Logo-------------------------------------------------------->
+
+                <a href="index.php" class="nav-logo"><img src="Gallery/Logo.jpg" alt="">
+<!--                    <h3 id="logo-name">fitness touch by mbijiwe</h3>-->
+                </a>
+
+                <ul class="nav-links">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#blog">Blog</a></li>
+                    <li><a href="#faqs">faqs</a></li>
+                    <li><a href="contact.php">contact us</a></li>
+                </ul>
+
+<!--------------------------------------------------ToggleMenu---------------------------------------------------->
+
+                <button class="menu-btn"><i class="uil uil-bars"></i></button>
+                <button class="close-btn"><i class="uil uil-multiply"></i></button>
+            </div>
+        </nav>
+
+<!------------------------------------------------Header Section-------------------------------------------------->
+
+        <header>
+                <div class="container header-container">
+                    <h1 id="header-title">Fitness <span>.</span> Health <span>.</span> Nutrition</h1>
+                        <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab culpa dolore eius, eos illo iure laboriosam minima
+                            molestiae nam nesciunt nostrum obcaecati, quam quia quibusdam repellat sunt tenetur vel. Temporibus.</p>
+
+                            <div class="header-image"><img src="" alt=""></div>
+
+                                <div class="header-socials">
+                                    <a href="https://facebook.com/apple" target="_blank"><i class="uil uil-facebook-f"></i></a>
+                                    <a href="https://instagram.com/apple" target="_blank"><i class="uil uil-instagram-alt"></i></a>
+                                    <a href="https://twitter.com/apple" target="_blank"><i class="uil uil-twitter"></i></a>
+                                    <a href="https://linkedin.com/apple" target="_blank"><i class="uil uil-linkedin-alt"></i></a>
+                                </div>
+                </div>
+
+                            <div class="header-deco-1"><img src="Gallery" alt=""></div>
+
+                            <div class="header-deco-2"><img src="Gallery" alt=""></div>
+        </header>
+
+<!-------------------------------------------------Main (About) Section-------------------------------------------------->
+        <section class="" id="about">
+            <h1 class="about-title">About Us</h1>
+                <div class="container">
+                    <article class="about-article" id="about-article-1" >
+                        <div class="about-image">
+                            <img src="Gallery/Stay-Fit2.jpg" alt="">
+                        </div>
+                            <div class="about-content">
+                                <h2 class="about-article-title">Learn how you can Exercise and stay fit with us.</h2>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eaque, explicabo, facere fugiat illo illum itaque maxime minima nemo quis sed voluptatibus.
+                                        <br><br>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eaque, explicabo, facere fugiat illo illum itaque maxime minima nemo quis sed voluptatibus.
+                                    </p>
+
+                            </div>
+                    </article>
+
+                    <article class="about-article">
+                        <div class="about-content">
+                            <h2 class="about-article-title">Learn how you can Exercise and stay fit with us.</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eaque, explicabo, facere fugiat illo illum itaque maxime minima nemo quis sed voluptatibus.
+                                Accusantium ipsum nemo neque numquam quis sequi.
+                                <br><br>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eaque, explicabo, facere fugiat illo illum itaque maxime minima nemo quis sed voluptatibus.
+                                Accusantium ipsum nemo neque numquam quis sequi.
+                            </p>
+                        </div>
+                        <div class="about-image">
+                            <img src="Gallery/240_I.jpg" alt="">
+                        </div>
+                    </article>
+
+                    <article class="about-article" id="about-article-3">
+                        <div class="about-image">
+                            <img src="Gallery/To_Do.png" id="seven" alt="">
+                        </div>
+                        <div class="about-content">
+                            <h2 class="about-article-title">Learn how you can Exercise and stay fit with us.</h2>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eaque, explicabo, facere fugiat illo illum itaque maxime minima nemo quis sed voluptatibus.
+                                <br><br>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore doloribus eaque, explicabo, facere fugiat illo illum itaque maxime minima nemo quis sed voluptatibus.
+
+                            </p>
+                        </div>
+                    </article>
+
+
+                </div>
+
+            <hr class="line">
+        </section>
+
+
+</body>
+</html>
