e44e0807392e77471eff0bfafd5b6ecdb3efada3
Charles-Muia <Charles.K.Muia@gmail.com>
2023-09-28 18:16:48 +0300

    FTM

--- index.php
+++ index.php
@@ -1,3 +1,94 @@
 <?php
 ?>
 
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Fitness By Mbijiwe | Home</title>
+
+<!--###---FAVICON------------------------------------------------------------------------------------------------------>
+    <link rel="icon" type="image" href="Gallery/Logo.jpg">
+
+<!--###---CSS-FILE----------------------------------------------------------------------------------------------------->
+    <link rel="stylesheet" href="style.css">
+
+<!--###---CDN-LINKS---------------------------------------------------------------------------------------------------->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js">
+
+<!--###---GOOGLE-FONTS------------------------------------------------------------------------------------------------->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fuggles&family=Lato&family=Mooli&family=News+Cycle&family=Orbitron&family=Quicksand&display=swap" rel="stylesheet">
+
+</head>
+    <body>
+
+    <section>
+
+    </section>
+
+<!--###---CONTACT-US--------------------------------------------------------------------------------------------------->
+    <section class="Contact-US-Sec" id="ContactUS">
+        <div class="Contact-US">
+            <div class="Contact-US-Title">
+                <h2>get in touch</h2>
+            </div>
+
+                <div class="Contact-box">
+                    <div class="Contact form">
+                        <h3>talk to us</h3>
+                            <form action="">
+                                <div class="contBox">
+                                    <div class="row50">
+                                        <div class="inputBox">
+                                            <span>first name</span>
+                                                <input type="text" placeholder="">
+                                        </div>
+                                                <div class="inputBox">
+                                                    <span>last name</span>
+                                                    <input type="text" placeholder="">
+                                                </div>
+                                    </div>
+                                        <div class="row50">
+                                            <div class="inputBox">
+                                                <span>email address</span>
+                                                    <input type="text" placeholder="">
+                                            </div>
+                                            <div class="inputBox">
+                                                <span>mobile No.</span>
+                                                <input type="text" placeholder="">
+                                            </div>
+                                        </div>
+
+                                    <div class="row100">
+                                        <div class="inputBox">
+                                            <span>message</span>
+                                            <textarea placeholder=""></textarea>
+                                        </div>
+                                    </div>
+
+                                    <div class="row100">
+                                        <div class="inputBox">
+                                            <input type="submit" value="click to send">
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </form>
+                    </div>
+
+                        <div class="Contact info"></div>
+                        <div class="Contact map"></div>
+
+                </div>
+        </div>
+    </section>
+
+    </body>
+</html>
+
