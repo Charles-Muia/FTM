40538c624198b132d0c32a4fa02ff870492852ad
Charles-Muia <Charles.K.Muia@gmail.com>
2023-09-29 11:54:54 +0300

    FTM

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 
 <!--###---CSS-FILE----------------------------------------------------------------------------------------------------->
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="responsive.css">
 
 <!--###---CDN-LINKS---------------------------------------------------------------------------------------------------->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
@@ -47,28 +48,28 @@
                                     <div class="row50">
                                         <div class="inputBox">
                                             <span>first name</span>
-                                                <input type="text" placeholder="">
+                                                <input type="text" placeholder="Enter Your First Name">
                                         </div>
                                                 <div class="inputBox">
                                                     <span>last name</span>
-                                                    <input type="text" placeholder="">
+                                                    <input type="text" placeholder="Enter Your Last Name">
                                                 </div>
                                     </div>
                                         <div class="row50">
                                             <div class="inputBox">
                                                 <span>email address</span>
-                                                    <input type="text" placeholder="">
+                                                    <input type="text" placeholder="Enter your Email Address">
                                             </div>
                                             <div class="inputBox">
                                                 <span>mobile No.</span>
-                                                <input type="text" placeholder="">
+                                                <input type="text" placeholder="Enter Your Mobile No.">
                                             </div>
                                         </div>
 
                                     <div class="row100">
                                         <div class="inputBox">
                                             <span>message</span>
-                                            <textarea placeholder=""></textarea>
+                                            <textarea placeholder="Type Your Message Here ................................. "></textarea>
                                         </div>
                                     </div>
 
@@ -82,12 +83,52 @@
                             </form>
                     </div>
 
-                        <div class="Contact info"></div>
-                        <div class="Contact map"></div>
+                        <div class="Contact info">
+                            <h3>find us</h3>
+                            <div class="infoBox">
+                                <div>
+                                    <span><i class="fa-solid fa-location-dot"></i></span>
+                                        <p> +254 Nairobi, KE (East Africa) </p>
+                                </div>
+                                    <div>
+                                        <span><i class="fa-solid fa-envelope"></i></span>
+                                        <a href="#">info@ftm.com</a>
+                                    </div>
+                                    <div>
+                                        <span><i class="fa-solid fa-phone"></i></span>
+                                        <a href="#">+254 720 000 000</a>
+                                    </div>
+
+                                        <ul class="social-media-icons">
+                                            <li><a href="#"><ion-icon name="logo-facebook"></ion-icon></a></li>
+                                            <li><a href="#"><i class="fa-brands fa-threads"></i></a></li>
+                                            <li><a href="#"><i class="fa-brands fa-twitter"></i></a></li>
+                                            <li><a href="#"><i class="fa-brands fa-x-twitter"></i></a></li>
+                                            <li><a href="#"><i class="fa-brands fa-linkedin-in"></i></a></li>
+                                            <li><a href="#"><i class="fa-brands fa-youtube"></i></a></li>
+                                        </ul>
+
+                            </div>
+                        </div>
+
+                        <div class="Contact map">
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8981.538432524514!2d37.60708051741044!3d55.751820745316586!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46b54a50b315e573%3A0xa886bf5a3d9b2e68!2sThe%20Moscow%20Kremlin!5e0!3m2!1sen!2ske!4v1695976072693!5m2!1sen!2ske"
+                                    style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+                        </div>
 
                 </div>
         </div>
     </section>
+
+
+
+
+<!--###---JS----------------------------------------------------------------------------------------------------------->
+
+        <!--ION-ICONS-->
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 
     </body>
 </html>
